304a92554c1946ff24af903fc87c4caf8812b47b
halfh07 <37528829+halfh07@users.noreply.github.com>
2018-05-01 15:58:52 -0500

    Initial commit

---
+++ src/starcraft/rpg/ZERG.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package starcraft.rpg;
+
+/**
+ *
+ * @author Alons
+ */
+public class ZERG 
+{
+    public float size;
+    public float cargo;
+    public int armor;
+    public int health;
+    public char attributos;
+    public int ataqueT;
+    public int ataqueA;
+    public int bonus;
+    public float cooldown;
+    public float speed;
+    public float creepbonus;
+    public int rango;
+    public int vista;
+}
+
